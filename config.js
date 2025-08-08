2993075c268401312e586d1a75c7655598416302
konacolab10 <konanyann835@gmail.com>
2025-08-08 19:43:25 +0000

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "YOUR SESSION_ID",
+SESSION_ID: process.env.SESSION_ID || "XTREME~XMD~e95BHbDQ#dVvR024jqBPQLkmrPLDs8ZT6dtobA-txYfBEVx0aDlc",
 
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
